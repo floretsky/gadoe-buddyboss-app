6290c8add784930a5c5d0e59803d461df59937e5
Anton Floretsky <afloretsky@gmail.com>
2024-01-30 14:27:01 -0300

    restored all to example

--- data.js
+++
@@ -1,2 +0,0 @@
-export const registrationPageUrl = 'https://gadoe:gadoe@community.clockworkwp.com/gadoe-community-registration/';
-export const registrationPageLabel = 'Create an Account - GaDOE Community';
--- index.js
+++ index.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import { View } from "react-native";
-import RegistrationScreen from './components/RegistrationScreen';
+import {Observable} from "rxjs";
+import React from "react";
+import {View} from "react-native";
+import PageScreen from '@src/containers/Custom/PageScreen';
 
 export const applyCustomCode = externalCodeSetup => {
-  externalCodeSetup.navigationApi.replaceScreenComponent(
-    'SignupScreen',
-    RegistrationScreen
-  );
+	// call custom code api here
+
+    externalCodeSetup.indexJsApi.addIndexJsFunction(() => {
+		console.log("function from external set upo");
+	});
+
+	const reducerName = "external reducer";
+
+	externalCodeSetup.reduxApi.addReducer(
+		reducerName,
+		(s = {type: "demo"}, a) => s
+	);
+
+	externalCodeSetup.reduxApi.addEpic("external epic", actions =>
+		Observable.empty()
+	);
+
+	externalCodeSetup.reduxApi.wrapEpic(
+		"loadHomeScreen",
+		epic => (actions, store, dep) => epic(actions, store, dep)
+	);
+
+	externalCodeSetup.reduxApi.wrapReducer(
+		"homeScreen",
+		reducer => (state = reducer(undefined, {type: "demo"}), a) =>
+			reducer(state, a)
+	);
+
+	externalCodeSetup.reduxApi.addPersistorConfigChanger(config => ({
+		...config,
+		whitelist: [...config.whitelist, reducerName]
+	}));
+
+	externalCodeSetup.navigationApi.addNavigatorCreatedCallback(navigator => {
+		//debugger;
+
+		let dispatch = navigator.dispatch;
+		console.log("navigator created");
+	});
+
+  const registrationPageUrl = 'https://gadoe:gadoe@community.clockworkwp.com/gadoe-community-registration/';
+  const registrationPageLabel = 'Create an Account - GaDOE Community';
+
+	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+		<View style={{ flex: 1 }}>
+      <PageScreen
+        url={registrationPageUrl}
+        openExternal={false}
+        label={registrationPageLabel}
+      />
+    </View>
+	));
+
+	externalCodeSetup.cssApi.addGlobalStyle("container", {
+		backgroundColor: "white"
+	});
 };
