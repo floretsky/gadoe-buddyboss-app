690691598bc5eeb901124c2a1ea3bb49a1b9bf15
Anton Floretsky <afloretsky@gmail.com>
2024-01-30 13:27:29 -0300

    replacement of sign in screen with in-app link to registration page

---
+++ data.js
@@ -0,0 +1,2 @@
+export const registrationPageUrl = 'https://gadoe:gadoe@community.clockworkwp.com/gadoe-community-registration/';
+export const registrationPageLabel = 'Create an Account - GaDOE Community';
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export const applyCustomCode = (externalCodeSetup: any) => {
-	// call custom code api here
+import RegistrationScreen from './components/RegistrationScreen';
+
+export const applyCustomCode = (externalCodeSetup) => {
+  // call custom code api here
+  externalCodeSetup.navigationApi.replaceScreenComponent(
+    'SignupScreen',
+    RegistrationScreen
+  );
 };
