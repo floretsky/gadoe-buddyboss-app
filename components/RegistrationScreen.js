38ca8b5b0453cb0a53a1fccdc005f4d024a9be81
Anton <afloretsky@gmail.com>
2024-11-25 16:58:01 -0300

    slight formatting

--- components/RegistrationScreen.js
+++ components/RegistrationScreen.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import {View} from "react-native";
 import { registrationPageUrl, registrationPageLabel } from '../data';
-import PageScreen from '@src/containers/Custom/PageScreen';
+
+import PageScreen from "@src/containers/Custom/PageScreen";
 
 const RegistrationScreen = (props) => (
-  <View style={{ flex: 1 }}>
+  <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
     <PageScreen
       url={registrationPageUrl}
       openExternal={false}
