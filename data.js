12d88394ebe68308233bc4030615dac2e80f6ae5
Anton <afloretsky@gmail.com>
2024-12-23 18:16:16 -0300

    restored community reg page url

--- data.js
+++ data.js
@@ -1,2 +1,2 @@
-export const registrationPageUrl = 'https://community.clockworkwp.com/gadoe-community-registration/';
+export const registrationPageUrl = 'https://community.gadoe.org/gadoe-community-registration/';
 export const registrationPageLabel = 'Create an Account - GaDOE Community';
